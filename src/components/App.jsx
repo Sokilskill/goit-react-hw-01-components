1ed1e2ee5ebd2f8d360850d6c69fc0fded0e261a
Sokilskill <vprovorotov@gmail.com>
2023-09-03 12:59:21 +0300

    add second component with module.css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,19 @@
 import user from 'path/to/user.json';
+import data from 'path/to/data.json';
 import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
 
 export const App = () => {
   return (
-    <Profile
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-    />
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Data Group" stats={data} />
+    </>
   );
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import css from './Statistics.module.css';
+
+function Statistics({ title, stats }) {
+  const renderTitle = title ? <h2 className={css.title}>{title}</h2> : null;
+
+  return (
+    <section className={css.statistics}>
+      {renderTitle}
+      <ul className={css.statList}>
+        {stats.map((item, index) => (
+          <li
+            key={item.id}
+            className={css.item}
+            style={{ backgroundColor: getColorByLabel(index) }}
+          >
+            <span className={css.label}>{item.label}</span>
+            <span className={css.percentage}>{item.percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
+
+function getColorByLabel(index) {
+  const colors = ['red', 'blue', 'green', 'orange', 'purple'];
+  const colorIndex = index % colors.length;
+  return colors[colorIndex];
+}
+export default Statistics;
